3d38ffc11ccc049033aa81cf564d88d9352bca8d
Tenzin Choerab <tlobchoerab@gmail.com>
2023-01-11 13:22:47 -0500

    added OP code

--- simpleVM.C
+++ simpleVM.C
@@ -15,5 +15,27 @@ enum{
     R_COUNT
 };
 
-uint16_t reg[R_COUNT];
+uint16_t reg[R_COUNT]; /* register storage */
+
+enum{
+    OP_BR = 0, /* branch */
+    OP_ADD,    /* add */
+    OP_LD,     /* load */
+    OP_ST,     /* store */
+    OP_JSR,    /* jump register */
+    OP_AND,    /* bitwise and */
+    OP_LDR,    /* load register */
+    OP_STR,    /* store register */
+    OP_RTI,    /* unused */
+    OP_NOT,    /* bitwise not */
+    OP_LDI,    /* load indirect */
+    OP_STI,    /* store indirect */
+    OP_JMP,    /* jump */
+    OP_RES,    /* reserved */
+    OP_LEA,    /* load effective address */
+    OP_TRAP    /* execute trap */
+};
+
+
+
 
