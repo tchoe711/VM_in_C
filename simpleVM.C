77e5e9b4118f72340efed111ad42234af6ad4719
Tenzin Choerab <tlobchoerab@gmail.com>
2023-01-11 14:19:10 -0500

    created skeleton for main method running opcode

--- simpleVM.C
+++ simpleVM.C
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX]; /* 65536 locations*/
 
@@ -42,6 +46,78 @@ enum {
     FL_NEG = 1 << 2  /* N */
 };
 
+int main( int argc, const char* argv[]) {
+    if (argc < 2)
+    {
+        /* show usage string */
+        printf("lc3 [image-file1] ...\n");
+        exit(2);
+    }
+
+    for (int j = 1; j < argc; ++j)
+    {
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+
+    signal(SIGINT, handle_interrupt);
+    disable_input_buffering();
+
+    /* since exactly once condition flag should be set at any given time, set the Z flag */
+    reg[R_COND] = FL_ZRO;
+    /* set the PC to starting position */
+    /* 0x3000 is the default */
+
+    enum { PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while (running) {
+        /* FETCH */
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+
+        switch (op) {
+            case OP_ADD:
+                break;
+            case OP_AND:
+                break;
+            case OP_NOT:
+                break;
+            case OP_BR:
+                break;
+            case OP_JMP:
+                break;
+            case OP_JSR:
+                break;
+            case OP_LD:
+                break;
+            case OP_LDI:
+                break;
+            case OP_LDR:
+                break;
+            case OP_LEA:
+                break;
+            case OP_ST:
+                break;
+            case OP_STI:
+                break;
+            case OP_STR:
+                break;
+            case OP_TRAP:
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+
+                breal;
+        }
+    }
+    restore_input_buffering();
+}
 
 
 
